1170d8341635ee0796a6bbd8a463311621ca06f0
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-09-05 17:05:11 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 10;
+const num4 = 6;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10);
+let mod = num3 % num4;
+let max = Math.max(1, 3, 8, 4, 13, 19, 20, 3);
